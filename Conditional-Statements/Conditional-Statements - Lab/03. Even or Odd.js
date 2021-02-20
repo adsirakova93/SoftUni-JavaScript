5a1dcb841d54223a76461819269beded51a7e907
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-02-20 23:37:58 +0200

    uploading solutions

---
+++ Conditional-Statements/Conditional-Statements - Lab/03. Even or Odd.js
@@ -0,0 +1,13 @@
+function even_or_odd(number) {
+
+    let num = Number(number);
+
+    if (num % 2 === 0) {
+        console.log("even");
+    } else {
+        console.log("odd");
+    }
+
+}
+
+even_or_odd(["2"])
---
+++ Conditional-Statements/Conditional-Statements - Lab/04. Number 100...200.js
@@ -0,0 +1,14 @@
+function check_number(num) {
+
+    let number = Number(num);
+
+    if (number < 100) {
+        console.log("Less than 100");
+    } else if (number >= 100 && number < 200) {
+        console.log("Between 100 and 200");
+    } else {
+        console.log("Greater than 200");
+    }
+}
+
+check_number(["95"])
