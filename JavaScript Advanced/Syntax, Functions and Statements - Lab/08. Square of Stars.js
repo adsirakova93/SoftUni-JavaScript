329701b4c8ead42bb37a0d5adb977f526e74eb0a
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-01 23:45:46 +0300

    uploading task 8

---
+++ JavaScript Advanced/Syntax, Functions and Statements - Lab/08. Square of Stars.js
@@ -0,0 +1,13 @@
+function squareOfStars(input) {
+    let stars = "* ";
+    let num = Number(input);
+
+    console.log(stars.repeat(num))
+}
+
+
+
+squareOfStars(1)
+squareOfStars(2)
+squareOfStars(3)
+squareOfStars(10)
