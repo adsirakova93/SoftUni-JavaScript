2540a71a1fcecaf1507907c5ef7af22926974d07
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-01 21:44:41 +0300

    uploading task 5

---
+++ JavaScript Advanced/Syntax, Functions and Statements - Lab/05. Math Operations.js
@@ -0,0 +1,21 @@
+function calculator(num1, num2, operator) {
+    let result = ""
+
+    if (operator == "+"){
+        result = num1 + num2;
+    } else if (operator == "-"){
+        result = num1 - num2;
+    } else if (operator == "*"){
+        result = num1 * num2;
+    } else if (operator == "/") {
+        results = num1 / num2;
+    } else if (operator == "%") {
+        result = num1 % num2;
+    } else if (operator == "**") {
+        result = num1 ** num2;
+    }
+    console.log(result)
+}
+
+calculator(5, 6, '+')
+calculator(3, 5.5, '*')
