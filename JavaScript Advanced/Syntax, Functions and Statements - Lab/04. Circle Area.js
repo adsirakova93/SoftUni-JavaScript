1224be8e54488a378a5c1f6c85ece69abde2bd01
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-01 21:33:47 +0300

    uploading task 4

---
+++ JavaScript Advanced/Syntax, Functions and Statements - Lab/04. Circle Area.js
@@ -0,0 +1,14 @@
+function circle(input) {
+    let typeInput = typeof(input);
+
+    if (typeInput === "number"){
+        let result = Math.PI * (input * input)
+        console.log(result.toFixed(2));
+    }
+    else {
+        console.log(`We can not calculate the circle area, because we receive a ${typeInput}.`)
+    }
+}
+
+circle(5);
+circle('name')
