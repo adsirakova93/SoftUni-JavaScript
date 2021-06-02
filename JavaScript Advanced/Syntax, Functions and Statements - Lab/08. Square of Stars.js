59101a9e383bb60a6cd762b8101fed5d5a2d9110
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-02 14:45:29 +0300

    update task 8

--- JavaScript Advanced/Syntax, Functions and Statements - Lab/08. Square of Stars.js
+++ JavaScript Advanced/Syntax, Functions and Statements - Lab/08. Square of Stars.js
@@ -2,12 +2,16 @@ function squareOfStars(input) {
     let stars = "* ";
     let num = Number(input);
 
-    console.log(stars.repeat(num))
+    if (input === undefined) {
+        num = 5
+    }
+
+    for (i = 1; i <= num; i++) {
+        console.log(stars.repeat(num));
+    }
 }
-
-
 
 squareOfStars(1)
 squareOfStars(2)
 squareOfStars(3)
-squareOfStars(10)
+squareOfStars()
