bdf15753478e878e094c2350f9fe5ca4f5a7ba98
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-02 15:10:27 +0300

    uploading task 9

---
+++ JavaScript Advanced/Syntax, Functions and Statements - Lab/09. Aggregate Elements.js
@@ -0,0 +1,16 @@
+function calculator(input) {
+    let elements = input.map(Number);
+    aggregate(elements, 0, (a, b)=>a + b);
+    aggregate(elements, 0, (a, b)=>a + 1 / b);
+    aggregate(elements, "", (a, b)=>a + b);
+ 
+    function aggregate(arr, initVal, func) {
+        let val = initVal;
+        for (let i = 0; i < arr.length; i++) {
+            val = func(val, arr[i]);
+        }
+        console.log(val);
+    }
+}
+calculator([1, 2, 3])
+calculator([2, 4, 8, 16])
