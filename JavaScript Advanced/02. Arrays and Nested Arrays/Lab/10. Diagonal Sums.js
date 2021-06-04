ba8da95c43dd9b367c2ced2940ff97d1297c23aa
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-04 23:35:02 +0300

    fix of the task 10

--- JavaScript Advanced/02. Arrays and Nested Arrays/Lab/10. Diagonal Sums.js
+++ JavaScript Advanced/02. Arrays and Nested Arrays/Lab/10. Diagonal Sums.js
@@ -1,17 +1,23 @@
 function solve(matrix) {
     let sum1 = 0;
     let sum2 = 0;
-    let matrixLength = Number(matrix[0].length)
+    let matrixLength = matrix[0].length
+    let result = [];
 
-    for (let i = 0; i < matrixLength; i++) {
-        sum1 += matrix[i][i];
-        sum2 += matrix[matrixLength - i][matrixLength - i];
+    for (let index = 0; index < matrixLength; index++) {
+        sum1 += matrix[index][index];
+        sum2 += matrix[index][(matrixLength - 1) - index];
     }
-
-    console.log(sum1);
-    console.log(sum2);
+    
+    result.push(sum1);
+    result.push(sum2);
+    return result.join(' ')
 }
 
-console.log(solve([[20, 40],
-[10, 60]]
-))
+console.log(solve([[20, 40], 
+    [10, 60]]));
+
+console.log(solve([[3, 5, 17],
+    [-1, 7, 14],
+    [1, -8, 89]]
+   ));
