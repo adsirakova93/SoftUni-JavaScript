13209baef0cf950d2ccc66785ed10959c368c41d
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-06 23:57:11 +0300

    task 3 fix

--- JavaScript Advanced/02. Arrays and Nested Arrays/03. Add and Remove Elements.js
+++ JavaScript Advanced/02. Arrays and Nested Arrays/03. Add and Remove Elements.js
@@ -1,12 +1,12 @@
 function addOrRemove(command) {
-    num = 1;
     newArray = [];
 
     for (let i = 0; i < command.length; i++) {
-        if (command == 'add') {
+        num = 1;
+        if (command[i] === 'add') {
             newArray.push(num);
             num += 1;
-        } else if (command == 'remove') {
+        } else if (command[i] === 'remove') {
             newArray.pop()
         }
     }
