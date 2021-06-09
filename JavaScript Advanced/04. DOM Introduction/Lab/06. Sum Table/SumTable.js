1006bd90e1ed71870ae3bfeabc11c2bfb42aab56
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-10 00:03:17 +0300

    DOM lab task 6 fix

--- JavaScript Advanced/04. DOM Introduction/Lab/06. Sum Table/SumTable.js
+++ JavaScript Advanced/04. DOM Introduction/Lab/06. Sum Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
+    let sum = 0;
 
+    let numbers = document.querySelectorAll('td');
+    let totalField = document.getElementById('sum')
+    for (let i = 0; i < numbers.length; i += 2) {
+        sum += Number(numbers[i+1].textContent);
+    }
+    totalField.textContent = sum;
 }
