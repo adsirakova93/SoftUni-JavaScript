c7ec8e5f98794e883b45ffc4de4f5a02f25d1a3d
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-13 23:44:03 +0300

    syntax task 6 fix

--- JavaScript Advanced/01. Syntax, Functions and Statements/06. Cooking by Numbers.js
+++ JavaScript Advanced/01. Syntax, Functions and Statements/06. Cooking by Numbers.js
@@ -1,24 +1,31 @@
-function solve(array) {
-    let [num, ...commands] = array
-    let nums = Number(num);
-    let result = [];
-
-    for (let element of commands) {
-        if (element === 'chop') {
-            nums /= 2;
-        } else if (element === 'dice') {
-            nums = Math.sqrt(nums);
-        } else if (element === 'spice') {
-            nums += 1;
-        } else if (element === 'bake') {
-            nums *= 3;
-        } else if (element === 'fillet') {
-            nums *= 0.8;
+function solve(...params) {
+    let num = Number(params[0]);
+    for (let i = 1; i < params.length; i++) {
+        switch (params[i]) {
+            case 'chop':
+                num /= 2;
+                console.log(num);
+                break;
+            case 'dice':
+                num = Math.sqrt(num);
+                console.log(num);
+                break;
+            case 'spice':
+                num += 1;
+                console.log(num);
+                break;
+            case 'bake':
+                num *= 3;
+                console.log(num);
+                break;
+            case 'fillet':
+                num -= num * 0.2;
+                console.log(num);
+                break;
+            default:
+                break;
         }
-        result.push(nums);
     }
-    return result.join('\n')
 }
 
-// console.log(solve(['32', 'chop', 'chop', 'chop', 'chop', 'chop']));
-console.log(solve(['9', 'dice', 'spice', 'chop', 'bake', 'fillet']))
+solve('9', 'dice', 'spice', 'chop', 'bake', 'fillet');
