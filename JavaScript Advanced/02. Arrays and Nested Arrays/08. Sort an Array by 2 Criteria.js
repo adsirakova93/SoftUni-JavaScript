957059e59dbe2b683425a8324e58fbfc18f4b3f5
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-13 22:57:40 +0300

    array task 8 solution

--- JavaScript Advanced/02. Arrays and Nested Arrays/08. Sort an Array by 2 Criteria.js
+++ JavaScript Advanced/02. Arrays and Nested Arrays/08. Sort an Array by 2 Criteria.js
@@ -3,6 +3,6 @@ function sort(array) {
     return result.join('\n');
 }
 
-// console.log(sort(['alpha', 'beta', 'gamma']));
-// console.log(sort(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']));
+console.log(sort(['alpha', 'beta', 'gamma']));
+console.log(sort(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']));
 console.log(sort(['test', 'Deny', 'omen', 'Default']));
