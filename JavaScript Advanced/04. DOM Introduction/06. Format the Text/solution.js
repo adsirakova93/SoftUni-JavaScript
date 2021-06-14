0f548226648b39bafed514537024daea3062272a
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-14 13:25:11 +0300

    DOM task 6 fix

--- JavaScript Advanced/04. DOM Introduction/06. Format the Text/solution.js
+++ JavaScript Advanced/04. DOM Introduction/06. Format the Text/solution.js
@@ -1,9 +1,10 @@
 function solve() {
   let textElement = document.getElementById('input');
-  let text = textElement.value.split('.');
+  let text = textElement.value.split('.').filter(x => x !== '').map(x => x + '.');
   let outputElement = document.getElementById('output');
+  let paraNums = Math.ceil(text.length / 3);
 
-  let para = document.createElement('P');
-  para.innerText = textElement.value;
-  outputElement.appendChild(para);
+  for (let i = 0; i < paraNums; i++) {
+    outputElement.innerHTML += `<p>${text.splice(0, 3).join('')}</p>`;
+  }
 }
