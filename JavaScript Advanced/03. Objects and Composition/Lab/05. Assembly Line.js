c76eafb1cb9d1c7fe2a7d1c8c405e986a5c241af
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-15 23:55:16 +0300

    objects task 5 final fix

--- JavaScript Advanced/03. Objects and Composition/Lab/05. Assembly Line.js
+++ JavaScript Advanced/03. Objects and Composition/Lab/05. Assembly Line.js
@@ -1,10 +1,11 @@
 function createAssemblyLine() {
     let result = {
+
         hasClima(myCar) {
             myCar.temp = 21;
             myCar.tempSettings = 21;
             myCar.adjustTemp = function adjustTemp() {
-                if (myCar.temp < myCar.tempSettings){
+                if (myCar.temp < myCar.tempSettings) {
                     myCar.temp += 1;
                 } else {
                     myCar.temp -= 1;
@@ -13,30 +14,33 @@ function createAssemblyLine() {
         },
 
         hasAudio(myCar) {
-            myCar.currentTrack = {name: null, artist: null};
+            myCar.currentTrack = { name: null, artist: null };
             myCar.nowPlaying = function nowPlaying() {
                 if (myCar.currentTrack !== 'null') {
-                    console.log(`Now playing '${currentTrack.name}' by ${currentTrack.artist}`)
+                    console.log(`Now playing '${currentTrack.name}' by ${currentTrack.artist}`);
                 }
             }
+        },
 
-
+        hasParktronic(myCar) {
+            myCar.checkDistance = function checkDistance(distance) {
+                if (distance < 0.1) {
+                    console.log('Beep! Beep! Beep!');
+                } else if (0.1 <= distance < 0.25) {
+                    console.log('Beep! Beep!');
+                } else if (0.25 <= distance < 0.5) {
+                    console.log('Beep!');
+                } else {
+                    console.log('');
+                }
+            }
         }
-
-
-
-
     }
+    return result
 }
 
-
 const assemblyLine = createAssemblyLine();
-
-const myCar = {
-    make: 'Toyota',
-    model: 'Avensis'
-};
-
+const myCar = { make: 'Toyota', model: 'Avensis' };
 
 assemblyLine.hasClima(myCar);
 console.log(myCar.temp);
