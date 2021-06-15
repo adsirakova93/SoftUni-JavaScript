269d868276d60d4577d2d5d81fe6e249a617863c
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-15 22:38:23 +0300

    arrays task 6 fix to sort case-insensitive

--- JavaScript Advanced/02. Arrays and Nested Arrays/06. List Of Names.js
+++ JavaScript Advanced/02. Arrays and Nested Arrays/06. List Of Names.js
@@ -1,9 +1,9 @@
 function sortingNames(names) {
-    let sortedNames = names.sort();
+    let sortedNames = names.sort((a, b) => a.localeCompare(b, undefined, {sensitivity: 'base'}))
 
     for (let i = 0; i < names.length; i++) {
         console.log(`${i + 1}.${sortedNames[i]}`)
     }
 }
 
-sortingNames(["John", "Bob", "Christina", "Ema"])
+sortingNames(["John", "christina", "Bob", "Ema"])
