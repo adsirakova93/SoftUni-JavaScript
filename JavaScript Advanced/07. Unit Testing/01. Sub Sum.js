52d9b820965c9b97705f3489fe154fc5040e792c
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-22 11:13:10 +0300

    unite test task 1 fix

--- JavaScript Advanced/07. Unit Testing/01. Sub Sum.js
+++ JavaScript Advanced/07. Unit Testing/01. Sub Sum.js
@@ -1,16 +1,16 @@
-function sumElements(array, startIndex, endIndex) {
-
+function subSum(array, startIndex, endIndex) {
     if (startIndex < 0) {
         startIndex = 0;
     };
 
-    let newArray = array.slice(startIndex, endIndex);
-    let result = (total, current) => total + current;
-
-    return newArray.reduce(result);
+    if (Array.isArray(array) == false) {
+        return NaN;
+    }
+    return array.slice(startIndex, endIndex + 1).reduce((a, c) => a + Number(c), 0);;
 }
 
-// console.log(sumElements([10, 20, 30, 40, 50, 60], 3, 300));
-console.log(sumElements([1.1, 2.2, 3.3, 4.4, 5.5], 0, 1));
-
-// module.exports = sumElements;
+console.log(subSum([10, 20, 30, 40, 50, 60], 3, 300));
+console.log(subSum([1.1, 2.2, 3.3, 4.4, 5.5], -3, 1));
+console.log(subSum([10, 'twenty', 30, 40], 0, 2));
+console.log(subSum([], 1, 2));
+console.log(subSum('text', 0, 2));
