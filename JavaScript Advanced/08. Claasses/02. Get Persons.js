9f66c60fbf148846ac85dfa1d6bb516be9357f05
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-24 23:39:13 +0300

    DOM task 1

---
+++ JavaScript Advanced/08. Claasses/02. Get Persons.js
@@ -0,0 +1,8 @@
+class Person {
+    constructor(firstName, lastName, age, email) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.email = email
+    }
+}
