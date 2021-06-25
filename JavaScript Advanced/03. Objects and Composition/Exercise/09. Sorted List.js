fce7e0ea36fc9daabbed19450769e172a2725dde
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-26 00:23:17 +0300

    cleaning folder

--- JavaScript Advanced/03. Objects and Composition/Exercise/09. Sorted List.js
+++ JavaScript Advanced/03. Objects and Composition/Exercise/09. Sorted List.js
@@ -25,10 +25,10 @@ function createSortedList() {
         get
     }
 }
-let list = createSortedList();
-list.add(5);
-list.add(6);
-list.add(7);
-console.log(list.get(1));
-list.remove(1);
-console.log(list.get(1));
+// let list = createSortedList();
+// list.add(5);
+// list.add(6);
+// list.add(7);
+// console.log(list.get(1));
+// list.remove(1);
+// console.log(list.get(1));
