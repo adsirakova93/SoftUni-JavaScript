73d17e322e116c098f66da7e1bec2b645ee8b6ac
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-26 01:03:46 +0300

    uploading gitignore

--- JavaScript Advanced/04. DOM Introduction/Exercise/08. Generate Report/report.js
+++ JavaScript Advanced/04. DOM Introduction/Exercise/08. Generate Report/report.js
@@ -1,3 +1,21 @@
 function generateReport() {
-    //TODO
+    let outputElement = document.querySelector('#output');
+    let tableHead = document.querySelectorAll('tr th');
+    let tableBody = document.querySelectorAll('tbody tr');
+
+    let output = [];
+
+    for (const row of tableBody) {
+        let rowObj = {};
+        for (let i = 0; i < row.children.length; i++) {
+            let key = tableHead[i].lastChild.name;
+            let val = row.children[i].textContent;
+            if (tableHead[i].lastChild.checked) {
+                rowObj[key] = val;
+            }
+        }
+        output.push(rowObj);
+    }
+
+    outputElement.textContent = JSON.stringify(output);
 }
