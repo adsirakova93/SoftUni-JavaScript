68c9380e20562640081c3fbdb298ef0483c24faa
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-06-25 09:31:58 +0300

    Objects task 2 solved

--- JavaScript Advanced/03. Objects and Composition/02. Construction Crew.js
+++ JavaScript Advanced/03. Objects and Composition/02. Construction Crew.js
@@ -1,17 +1,25 @@
-function crew(data) {
-    let worker = {
-        weight: crew[weigth],
-        experience: data[2],
-        levelOfHydrated: data[3],
-        dizziness: data[3],
-    };
-    return worker;
+function worker(obj) {
+    if (obj.dizziness == false) {
+        return obj
+    }
+
+    let needWater = obj.weight * obj.experience * 0.1;
+    obj.levelOfHydrated += needWater;
+    return obj
 }
 
-
-console.log(crew({
+console.log(worker({
     weight: 80,
     experience: 1,
     levelOfHydrated: 0,
     dizziness: true
-}))
+}
+));
+
+console.log(worker({
+    weight: 95,
+    experience: 3,
+    levelOfHydrated: 0,
+    dizziness: false
+}
+));
