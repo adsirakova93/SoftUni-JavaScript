1cdeeb5756600b1cb4a7c342204aaf6cee9bf34c
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-10-19 21:34:48 +0300

    Adding new exam with 60/100

---
+++ JavaScript Advanced/Exam Preparation/JS Advanced - Exam Prep 2 - 15.02.2021/01. Christmas Gifts Delivery/app.js
@@ -0,0 +1,55 @@
+function solution() {
+    document.querySelector('button').addEventListener('click', addGifts);
+
+
+    function addGifts() {
+        let input = document.querySelector('input');
+        let giftName = input.value;
+        sendGifts(giftName);
+        input.value = ''
+    }
+    
+    function sendGifts(giftName) {
+        let giftPannel = document.querySelectorAll('section')[1];
+        let giftUi = giftPannel.querySelector('ul');
+        let newGiftLi = document.createElement('li');
+        newGiftLi.classList.add('gift');
+        newGiftLi.textContent = giftName;
+        
+        let sendButton = document.createElement('button');
+        sendButton.textContent = 'Send';
+        sendButton.id = "sendButton";
+        newGiftLi.appendChild(sendButton);
+        let discardButton = document.createElement('button');
+        discardButton.textContent = 'Discard';
+        discardButton.id = 'discardButton';
+        newGiftLi.appendChild(discardButton);
+        giftUi.appendChild(newGiftLi);
+        sendButton.addEventListener('click', () => sendingEl(giftName));
+        discardButton.addEventListener('click', () => discardingEl(giftName))
+    
+        let allLis = Array.from(document.querySelectorAll('.gift'));
+        allLis
+        .sort((a, b) => a.textContent.localeCompare(b.textContent))
+        .forEach(li => giftUi.appendChild(li)
+        );
+    }
+    
+    function sendingEl(giftName) {
+        let sentPannel = document.querySelectorAll('section')[2];
+        let currentUl = sentPannel.querySelector('ul');
+        let newLi = document.createElement('li');
+        newLi.textContent = giftName;
+        currentUl.appendChild(newLi);
+    }
+    
+    function discardingEl(giftName) {
+        let sentPannel2 = document.querySelectorAll('section')[3];
+        let currentUl2 = sentPannel2.querySelector('ul');
+        let newLi2 = document.createElement('li');
+        newLi2.textContent = giftName;
+        currentUl2.appendChild(newLi2);
+    }
+
+}
+
